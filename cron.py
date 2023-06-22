d2040093a50a718cd24bb53f2ef24e419f4aed8e
ippishio <waterlava535@gmail.com>
2023-06-22 19:54:24 +0300

    updated cron.py

--- cron.py
+++ cron.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 requests.post("http://127.0.0.1:5000/updateLogs")
-print(requests.get("http://127.0.0.1:5000/getEntries?from=all"))
+print(json.loads(requests.get("http://127.0.0.1:5000/getEntries?from=all").text))
