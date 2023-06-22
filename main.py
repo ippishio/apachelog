c962fb0179ab54e3c48fbe9112f7ebffaa608b17
ippishio <waterlava535@gmail.com>
2023-06-22 18:41:21 +0300

    init

---
+++ main.py
@@ -0,0 +1,77 @@
+import configparser
+from apachelogs import LogParser
+import psycopg2
+import os
+from flask import Flask, jsonify, request
+from datetime import datetime
+
+config = configparser.ConfigParser()
+config.read('apachelog.conf')
+parser = LogParser(config['DEFAULT']['ApacheLogFormat'])
+app = Flask(__name__)
+
+
+@app.route('/updateLogs', methods=['POST'])
+def updateLogs():
+    try:
+        conn = psycopg2.connect(dbname=config['DEFAULT']['DBName'],
+                                user=config['DEFAULT']['DBUsername'], host=config['DEFAULT']['DBHostname'], port=config['DEFAULT']['DBPort'], password=config['DEFAULT']['DBPassword'])
+        cursor = conn.cursor()
+    except BaseException as e:
+        print("Can't connect to database: "+str(e))
+        exit()
+
+    for file in os.listdir(config['DEFAULT']['ApacheLogPath']):
+        if "access.log" in file:
+            try:
+                f = open(config['DEFAULT']['ApacheLogPath'] +
+                         ('/' if config['DEFAULT']['ApacheLogPath'][-1] != '/' else '')+file)
+                for line in f.readlines():
+                    entry = parser.parse(line)
+                    cursor.execute("INSERT INTO logs SELECT %s, %s WHERE NOT EXISTS (SELECT address, date FROM logs WHERE address = %s AND date = %s);",
+                                   (entry.remote_host, entry.request_time, entry.remote_host, entry.request_time,))
+                    conn.commit()
+                f.close()
+            except BaseException as e:
+                print(
+                    f"Can't open Apache log at '{config['DEFAULT']['ApacheLogPath']}{'/' if config['DEFAULT']['ApacheLogPath'][-1] != '/' else ''}{file}': "+str(e))
+                exit()
+    cursor.close()
+    conn.close()
+    response = jsonify({'status': 'ok'})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/getEntries', methods=['GET'])
+def getEntries():
+    try:
+        conn = psycopg2.connect(dbname=config['DEFAULT']['DBName'],
+                                user=config['DEFAULT']['DBUsername'], host=config['DEFAULT']['DBHostname'], port=config['DEFAULT']['DBPort'], password=config['DEFAULT']['DBPassword'])
+        cursor = conn.cursor()
+    except BaseException as e:
+        print("Can't connect to database: "+str(e))
+        exit()
+    if (request.args.get("from") != "all"):
+        print(request.args.get("from"), request.args.get("to"),)
+        fromDate = datetime.strptime(
+            request.args.get("from"), '%Y-%m-%d-%H-%M-%S')
+        toDate = datetime.strptime(request.args.get("to"), '%Y-%m-%d-%H-%M-%S')
+        cursor.execute(
+            "SELECT * FROM logs WHERE date >= %s and date <  %s", (fromDate, toDate,))
+    else:
+        cursor.execute(
+            "SELECT * FROM logs")
+    json = []
+    ans = cursor.fetchall()
+    for addrs in ans:
+        json.append({"address": addrs[0], "date": addrs[1]})
+    response = jsonify(json)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    cursor.close()
+    conn.close()
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Apache log viewer
+
+## Getting started
+
+### Creating a database
+
+```console
+foo@bar:~& sudo apt install postgresql postgresql-contrib
+foo@bar:~& sudo -i -u postgres
+postgres=# createdb apachelog
+```
